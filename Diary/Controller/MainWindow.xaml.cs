7921071c5b42e261334ff0db1c72fddb44978616
BadWolf03 <tspam765@gmail.com>
2021-06-04 18:59:19 +0300

    delete some usings

--- Diary/Controller/MainWindow.xaml.cs
+++ Diary/Controller/MainWindow.xaml.cs
@@ -1,17 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
 using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 namespace Diary
 {
@@ -20,13 +12,13 @@ namespace Diary
     /// </summary>
     public partial class MainWindow : Window
     {
-        ApplicationContext db;
-        DateTime dt;
-        List<User> list = new List<User>();
-        List<User> endedTaskList = new List<User>();
+        private ApplicationContext db;
+        private DateTime dt;
+        private List<User> list = new List<User>();
+        private List<User> endedTaskList = new List<User>();
 
-        List<User> newtemplist = new List<User>();
-        List<User> templist = new List<User>();
+        private List<User> newtemplist = new List<User>();
+        private List<User> templist = new List<User>();
         public MainWindow()
         {
             InitializeComponent();
